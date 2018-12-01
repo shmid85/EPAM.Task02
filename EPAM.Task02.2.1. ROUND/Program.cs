6217324ae81589cec5e168255b96b024c9a0bfe9
Александр Мартынюк <shmid1985@outlook.com>
2018-12-01 18:08:14 +0400

    Добавьте файлы проекта.

---
+++ EPAM.Task02.2.1. ROUND/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM.Task02._2._1.ROUND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
